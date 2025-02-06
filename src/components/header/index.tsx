9bc25024d2e514af1ae892e9474734ab1e3f1a37
kayamazlum <mazlum09kaya@gmail.com>
2025-02-06 21:42:40 +0300

    menu yapimina basladim

---
+++ src/components/header/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Menu from "../menu";
+
+const Header = () => {
+  return (
+    <div className="w-full bg-zinc-900 flex justify-center">
+      <div className="w-[900px] flex justify-between h-14 items-center">
+        <span className="text-3xl font-bold">BLOG</span>
+        <Menu />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/menu/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Menu = () => {
+  return (
+    <div className="flex gap-6 text-lg">
+      <span>Blog</span>
+      <span>About</span>
+    </div>
+  );
+};
+
+export default Menu;
