6fed1e5c169b55d6d9eb6349fda82d443be28947
kayamazlum <mazlum09kaya@gmail.com>
2025-02-17 23:23:44 +0300

    user profile sayfası responsive hale getirildi

--- src/app/(pages)/user_profile/page.tsx
+++ src/app/(pages)/user_profile/page.tsx
@@ -12,26 +12,31 @@ const UserProfile = () => {
         <div className="w-[1200px] md:mx-8 sm:mx-4 mx-2 p-4">
           <div className="flex flex-col gap-4">
             <div className="w-full relative">
-              <div className="w-full bg-green-800 h-96 items-center justify-center flex rounded-md">
+              <div className="w-full bg-green-800 lg:h-96 md:h-72 sm:h-60 h-52  items-center justify-center flex rounded-md">
                 Header Image
               </div>
-              <div className="absolute -translate-y-36 translate-x-4 flex gap-2 items-center justify-center">
-                <div className="rounded-full h-32 w-32 border flex justify-center items-center text-light text-lg bg-zinc-900">
+              <div className="absolute xl:-translate-y-36 lg:-translate-y-32 md:-translate-y-28 sm:-translate-y-24 -translate-y-16 translate-x-4 flex gap-2 items-center justify-center">
+                <div className="rounded-full xl:size-32 lg:size-28 md:size-24 sm:size-20 size-14 border flex justify-center items-center text-light text-lg bg-zinc-900">
                   User
                 </div>
-                <div className="w-32 bg-black bg-opacity-50 px-4 py-2 rounded-md flex flex-col">
-                  <span className="text-light text-lg">John Doe</span>
-                  <span className="text-zinc-300 text-sm">@johndoe47</span>
+                <div className="w-32 bg-black bg-opacity-50 px-4 md:py-2 py-1 rounded-md flex flex-col">
+                  <span className="text-light lg:text-lg  ">John Doe</span>
+                  <span className="text-zinc-300 lg:text-sm text-xs">
+                    @johndoe47
+                  </span>
                 </div>
               </div>
             </div>
-            <div className="flex p-2 gap-4 dark:bg-darkultra bg-light2 rounded-md  items-center">
-              <div className="flex gap-2 items-center">
-                <FaLocationDot size={20} /> <span>New York, USA</span>
+            <div className="flex md:flex-row flex-col p-4 gap-4 dark:bg-darkultra bg-light2 rounded-md  items-center">
+              <div className="flex gap-2 items-center whitespace-nowrap">
+                <FaLocationDot size={20} /> <p>New York, USA</p>
               </div>
               <p className="border-l-2  border-dark dark:border-light2 pl-4">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Voluptatum sunt quasi est quaerat alias
+                Voluptatum sunt quasi est quaerat alias Lorem ipsum dolor sit
+                amet consectetur adipisicing elit. VoluLorem ipsum dolor sit
+                amet consectetur adipisicing elit. Voluptatum sunt quasi est
+                quaerat aliasptatum sunt quasi est quaerat alias
               </p>
             </div>
             <div className="text-2xl mt-8 mb-4 flex w-full justify-center items-center">
