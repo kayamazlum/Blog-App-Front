fcf407d2b0ef5453f4436642c87ca3783a4e20e5
kayamazlum <mazlum09kaya@gmail.com>
2025-02-20 22:20:00 +0300

    register sayfasi yapildi

---
+++ src/app/(pages)/register/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import RegisterC from "@/components/registerC";
+const Register = () => {
+  return (
+    <div className="bg-gradient-to-t to-dark2 from-10% from-indigo-900 w-full h-screen flex justify-center items-center text-light">
+      <RegisterC />
+    </div>
+  );
+};
+
+export default Register;
--- src/components/registerC/index.tsx
+++ src/components/registerC/index.tsx
@@ -2,18 +2,34 @@
 import React, { useState } from "react";
 import { Eye, EyeOff } from "lucide-react";
 
-const Login = () => {
+const RegisterC = () => {
   const [showPassword, setShowPassword] = useState(false);
   return (
-    <div className="rounded-md flex flex-col py-10 px-14 bg-dark border border-blue bg-opacity-50 text-light">
+    <div className="rounded-md flex flex-col py-10 px-14 bg-dark border border-blue bg-opacity-50">
       <div className="flex flex-col gap-4">
         <div className="w-full flex text-xl font-bold mb-4 border-b pb-2 justify-center items-center">
-          <span>LOGIN</span>
+          <span>REGISTER</span>
+        </div>
+        <div className="flex flex-col gap-2">
+          <label htmlFor="">Fullname</label>
+          <input
+            type="text"
+            placeholder="Fullname"
+            className="rounded-md p-2 bg-transparent border border-blue"
+          />
         </div>
         <div className="flex flex-col gap-2">
           <label htmlFor="">Username</label>
           <input
             type="text"
+            placeholder="Username"
+            className="rounded-md p-2 bg-transparent border border-blue"
+          />
+        </div>
+        <div className="flex flex-col gap-2">
+          <label htmlFor="">Email</label>
+          <input
+            type="email"
             placeholder="Username"
             className="rounded-md p-2 bg-transparent border border-blue"
           />
@@ -32,16 +48,14 @@ const Login = () => {
             {showPassword ? <EyeOff size={18} /> : <Eye size={18} />}
           </button>
         </div>
-        <span className="text-xs w-full text-right cursor-pointer hover:underline ">
-          Forgot your password?
-        </span>
+
         <button className="bg-blue rounded-md p-2 mt-2 hover:bg-orange-600 duration-500 transition">
-          Login
+          Register
         </button>
         <div className="text-sm flex gap-2 mt-2">
-          <span>Dont have an account?</span>
+          <span>Do you have an account?</span>
           <span className="font-bold underline cursor-pointer hover:text-blue duration-300 transition">
-            Sign up now
+            Sign in now
           </span>
         </div>
       </div>
@@ -49,4 +63,4 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default RegisterC;
